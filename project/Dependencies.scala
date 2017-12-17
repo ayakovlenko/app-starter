7c9b89aac82d6c6912cb63dbd608a5c738ed07f0
Anton Iacovlenco <anton.iacovlenco@codefactorygroup.com>
2017-12-17 19:49:22 +0200

    Add some libraries

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,4 +5,10 @@ object Dependencies {
   lazy val scalatest = "org.scalatest" %% "scalatest" % "3.0.4" % Test
 
   lazy val json4sJackson = "org.json4s" %% "json4s-jackson" % "3.5.3"
+
+  lazy val typesafeConfig = "com.typesafe" % "config" % "1.3.2"
+
+  lazy val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2"
+
+  lazy val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.2.3"
 }
