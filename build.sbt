a24456ede8ddaf9de447046f5e0a02fba2a24db4
tony <ayakovlenko@users.noreply.github.com>
2018-06-30 13:43:19 +0300

    Update versions

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ name := "app-starter"
 
 version := "0.1"
 
-scalaVersion := "2.12.4"
+scalaVersion := "2.12.6"
 
 lazy val root = (project in file("."))
   .aggregate(app, core)
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  lazy val scalatest = "org.scalatest" %% "scalatest" % "3.0.4" % Test
+  lazy val scalatest = "org.scalatest" %% "scalatest" % "3.0.5" % Test
 
   lazy val json4sJackson = "org.json4s" %% "json4s-jackson" % "3.5.3"
 
